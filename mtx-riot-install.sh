3e15cc76eaa79c632e8464800d167d83b32953bf
GrigRUSS <grigruss@ya.ru>
2017-07-17 10:18:32 +0300

    Let's Encrypt cert and config files.
    Added a certificate Let's Encrypt and setting configuration files.

--- mtx-riot-install.sh
+++ mtx-riot-install.sh
@@ -69,6 +69,7 @@ instif(){
     if [ $inst == "y" ]
     then
 	echo
+	apt-get install python-certbot-apache -t jessie-backports
 	apt-get install jq curl matrix-synapse
     elif [ $inst != "n" ]
     then
@@ -87,7 +88,6 @@ riotq(){
     echo
     echo "Do you want to install Riot on your site now? [Y/n]"
     read -n 1 riot
-    riotif
 }
 
 echo
@@ -125,6 +125,211 @@ echo "Download latest version of Riot-web"
 echo
 echo "Matrix-synapse and Riot-web are installed."
 echo
+
+leq(){
+    echo
+    echo "Do you want to receive and configure the Let's Encrypt certificate for your server? [Y/n]"
+    read -n 1 le
+    leif
+}
+lem(){
+    echo
+    echo "Enter the domain name for matrix-synapse:"
+    read led
+    if [ -z $led ]
+    then
+	ler
+    else
+	certbot certonly -d $led -d www.$led
+	ler
+    fi
+}
+ler(){
+    echo
+    echo "Do you want to use $led for Riot-web?:"
+    read -n 1 ledr
+    if [ $ledr == "y" ]
+    then
+	echo "Ok. Use $led."
+	lerd=$led
+    elif [ $ledr != "n" ]
+    then
+	echo
+	echo "Only \"y\" or \"n\""
+	ler
+    else
+	echo
+	echo "Enter the domain name for Riot-web:"
+	read lerd
+	if [ -z $lerd ]
+	then
+	    ler
+	else
+	    certbot certonly -d $lerd -d www.$lerd
+	fi
+    fi
+}
+leif(){
+    if [ $le == "y" ]
+    then
+	lem
+    elif [ $le != "n" ]
+    then
+	echo
+	echo "Only \"y\" or \"n\""
+	leq
+    else
+	echo
+	echo "Action of receive and configure certificate canceled."
+    fi
+}
+leq
+
+echo "Domain name for matrix-synapse: $led"
+echo "Domain name for Riot-web: $lerd"
+
+python -m synapse.app.homeserver --server-name $led --config-path homeserver.yaml --generate-config --report-stats=yes
+cp /etc/letsencrypt/archive/$led/* /etc/matrix-synapse/
+
+mtxconf(){
+    HFILE="/etc/matrix-synapse/homeserver.yaml"
+    while read LINE; do
+	if [[ $LINE == tls_certificate_path* ]]
+	then
+	    echo "# $LINE">>$HFILE.new
+	    echo "tls_certificate_path: \"/etc/matrix-synapse/cert1.pem\"">>$HFILE.new
+	elif [[ $LINE == tls_private_key_path* ]]
+	then
+	    echo "# $LINE">>$HFILE.new
+	    echo "tls_certificate_path: \"/etc/matrix-synapse/privkey1.pem\"">>$HFILE.new
+	elif [[ $LINE == tls_dh_params_path* ]]
+	then
+	    echo "# $LINE">>$HFILE.new
+	    echo "tls_certificate_path: \"/etc/matrix-synapse/chain1.pem\"">>$HFILE.new
+	else
+	    echo $LINE>>$HFILE.new
+	fi
+    done < $HFILE
+    mv $HFILE $HFILE.old
+    mv $HFILE.new $HFILE
+    echo
+    echo "Old homeserver.yaml "
+}
+mtxconfq(){
+    echo
+    echo "Do you want to configure matrix-synapse? [y/n]"
+    read -n 1 mtxc
+    if [ $mtxc == "y" ]
+    then
+	if [ $le == "y" ]
+	then
+	    mtxconf
+	fi
+	echo "homeserver.yaml configured"
+    elif [ $mtxc != "n" ]
+    then
+	echo
+	echo "Only \"y\" or \"n\""
+	leq
+    else
+	echo
+	echo "Action canceled."
+    fi
+}
+mtxconfq
+
+riotconf(){
+    HFILE="$WWW/config.sample.json"
+    CFILE="$WWW/config.json"
+    while read LINE; do
+	if [[ $LINE == *default_hs_url* ]]
+	then
+	    echo "	\"default_hs_url\": \"https://$led:8448\",">>$CFILE
+	elif [[ $LINE == *default_is_url* ]]
+	then
+	    echo
+	    echo "Do you want to use your server as an authentication server? [y/n]"
+	    read -n 1 ids
+	    if [ $ids == "y" ]
+	    then
+		echo "	\"default_is_url\": \"https://$led:8448\",">>$CFILE
+	    else
+		echo $LINE>>$CFILE
+	    fi
+	elif [[ $LINE == *\"matrix.org\"* ]]
+	then
+	    echo "			\"$lerd\",">>$CFILE
+	    echo $LINE>>$CFILE
+	else
+	    echo $LINE>>$CFILE
+	fi
+    done < $HFILE
+    echo
+}
+riotconfq(){
+    echo
+    echo "Do you want to configure Riot-web? [y/n]"
+    read -n 1 riotc
+    if [ $mtxc == "y" ]
+    then
+	if [ $le == "y" ]
+	then
+	    mtxconf
+	fi
+	echo "config.json configured"
+    elif [ $mtxc != "n" ]
+    then
+	echo
+	echo "Only \"y\" or \"n\""
+	leq
+    else
+	echo
+	echo "Action canceled."
+    fi
+}
+
+echo
+echo "Setting up a website for Riot-web."
+rcf="/etc/apache2/sites-avialable/$lerd.conf"
+echo "<VirtualHost msg.miacnao.ru:80>">>$rcf
+echo "RewriteEngine on">>$rcf
+echo "RewriteCond %{SERVER_NAME} =www.$lerd [OR]">>$rcf
+echo "RewriteCond %{SERVER_NAME} =$lerd">>$rcf
+echo "RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,QSA,R=permanent]">>$rcf
+echo "</VirtualHost>">>$rcf
+echo "">>$rcf
+echo "<IfModule mod_ssl.c>">>$rcf
+echo "<VirtualHost $lerd:443>">>$rcf
+echo "    ServerAdmin admin@$lerd">>$rcf
+echo "    ServerName $lerd">>$rcf
+echo "    ServerAlias www.$lerd">>$rcf
+echo "    DocumentRoot \"$WWW\"">>$rcf
+echo "    DirectoryIndex index.html">>$rcf
+echo "    ErrorLog \"/var/logs/apache2/error.$lerd.log\"">>$rcf
+echo "    CustomLog \"/home/www/httpd-logs/access.msg.miacnao.ru.log\" common">>$rcf
+echo "    SSLCertificateFile /etc/letsencrypt/live/$lerd/fullchain.pem">>$rcf
+echo "    SSLCertificateKeyFile /etc/letsencrypt/live/$lerd/privkey.pem">>$rcf
+echo "    Include /etc/letsencrypt/options-ssl-apache.conf">>$rcf
+echo "</VirtualHost>">>$rcf
+echo "<Directory $WWW>">>$rcf
+echo "    Options -Indexes">>$rcf
+echo "</Directory>">>$rcf
+echo "</IfModule>">>$rcf
+
+echo "All done!"
+echo "Config files:"
+echo "matrix-synapse - /etc/matrix-synapse/homeserver.yaml"
+echo "Riot-web - $WWW/config.json"
+echo "site for Riot-web - /etc/apache2/sites-avialable/$lerd.conf"
+echo
+echo "============================================================================"
+echo "Check the configuration files, and if everything is OK, run the following commands to start the server:"
+echo "    systemctl restart matrix-synapse"
+echo "    systemctl restart apache2"
+echo
+echo "And follow the link: https://$lerd"
+echo "============================================================================"
+echo
 echo "To automatically update the Riot-web, add the following line to cron (crontab -e):"
 echo "============================================================================"
 p=$(pwd)
@@ -133,3 +338,4 @@ echo "==========================================================================
 echo "This line will cause the update to run every day at 00:00."
 echo "The update will only be performed if a new version of Riot-web is available."
 echo
+
